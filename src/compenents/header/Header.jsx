a5b5456c609e50d4ed47dcf23b426312ebd2ce11
Felipe Daher <felipe_daher29@hotmail.com>
2022-02-25 10:02:35 -0300

    criado componente de icone

--- src/compenents/header/Header.jsx
+++ src/compenents/header/Header.jsx
@@ -1,6 +1,7 @@
 import styles from "./Header.module.css"
 import Title from "../title/title"
 import Subtitle from "../subtitle/subtitle"
+import Icon from "../icon/icon"
 
 function Header (){
   return (
@@ -8,6 +9,9 @@ function Header (){
       <div className={styles.header}>
         <Title>Jogo da Velha</Title>
         <Subtitle>Criado por Felipe Daher</Subtitle>
+        <div className={styles.icon}>
+          <Icon iconName="github" link="https://github.com/feDaher" />
+        </div>
       </div>
     </>
   )
